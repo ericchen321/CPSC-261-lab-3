75ab7e947c59bed3e405cbc03638caaa7a4f1d81
eric <cgx1997@outlook.com>
2019-01-26 22:50:33 -0800

    track optimal test files

--- file_auto_gen.sh
+++ file_auto_gen.sh
@@ -19,3 +19,12 @@ do
   printf "\n" >> tests/maxsa_reorder_array_$i.ys
   cat stack_end.txt >> tests/maxsa_reorder_array_$i.ys
 done
+
+for i in 0 1 2 3 4 5 6 7
+do
+  cp maxsa_optimal.ys tests/maxsa_optimal_array_$i.ys
+  printf "\n\n" >> tests/maxsa_optimal_array_$i.ys
+  cat array_$i.txt >> tests/maxsa_optimal_array_$i.ys 
+  printf "\n" >> tests/maxsa_optimal_array_$i.ys
+  cat stack_end.txt >> tests/maxsa_optimal_array_$i.ys
+done
