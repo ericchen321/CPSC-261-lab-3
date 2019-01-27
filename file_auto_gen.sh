41ba89d9952d1abd4e8dbfe76636e27f227a741f
eric <cgx1997@outlook.com>
2019-01-26 17:44:18 -0800

    added test case 2, array with 31 elements

--- file_auto_gen.sh
+++ file_auto_gen.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
 rm tests/*
-cp maxsa_reorder.ys tests/maxsa_reorder_array0.ys
-printf "\n\n" >> tests/maxsa_reorder_array0.ys
-cat array_0.txt >> tests/maxsa_reorder_array0.ys 
-printf "\n" >> tests/maxsa_reorder_array0.ys
-cat stack_end.txt >> tests/maxsa_reorder_array0.ys
+
+for i in 0 1 2
+do
+  cp maxsa_ref.ys tests/maxsa_ref_array$i.ys
+  printf "\n\n" >> tests/maxsa_ref_array$i.ys
+  cat array_$i.txt >> tests/maxsa_ref_array$i.ys 
+  printf "\n" >> tests/maxsa_ref_array$i.ys
+  cat stack_end.txt >> tests/maxsa_ref_array$i.ys
+done
+
+for i in 0 1 2
+do
+  cp maxsa_reorder.ys tests/maxsa_reorder_array$i.ys
+  printf "\n\n" >> tests/maxsa_reorder_array$i.ys
+  cat array_$i.txt >> tests/maxsa_reorder_array$i.ys 
+  printf "\n" >> tests/maxsa_reorder_array$i.ys
+  cat stack_end.txt >> tests/maxsa_reorder_array$i.ys
+done
